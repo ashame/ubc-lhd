10b288f0e0876aabb470994236fea73b1614dc6e
ashame <nathan@imashamed.net>
2018-12-01 14:31:58 -0800

    backup

--- components/screens/Settings.js
+++ components/screens/Settings.js
@@ -1,33 +1,20 @@
 import React, { Component } from 'react';
 import {
     StyleSheet,
-    Image,
     Text,
-    TouchableOpacity,
     View,
-    Slider, 
 } from 'react-native';
 import { Constants } from 'expo';
 
-export default class Profile extends Component {
+export default class HomeScreen extends Component {
     static navigationOptions = {
-        title: "Profile"              
+        title: "Settings"              
     };
     render() {
         const { navigate } = this.props.navigation;
         return (
             <View style={styles.container}>
-                <Text>Profile</Text>
-
-                <Text>I am a</Text>
-                <Text>Seeking a </Text>
-
-                <Slider
-                    style = {{ width: 300 }}
-                    step = {1}
-                    minimumValue = {0}
-                    maximumValue = {100}
-                />
+                <Text>Settings</Text>
             </View>
         )
     }
